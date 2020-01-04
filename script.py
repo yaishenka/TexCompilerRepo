bfb9dcf4061edfeaa7ea733f22692136b9f99891
yaishenka <gagarinovdaniil@gmail.com>
2020-01-04 23:58:37 +0300

    update travis

--- script.py
+++ script.py
@@ -6,8 +6,7 @@ compiled_dir = 'compiled/'
 file_list = os.listdir(sources_dir)
 
 for file in file_list:
-    if '.' in file or 'DS_Store' in file:
-        file_list.remove(file)
-
-for file in file_list:
+	if 'DS_Store' in file: # Костыль
+		continue
+	print(file)
 	os.system('pdflatex -output-directory={0} {1}'.format(compiled_dir, os.path.join(sources_dir, file)))
